8b1926cfce2ca7beb736fb0fecacf76c7fc67769
aaaaaaaaaaa <aaaaa>
2020-10-19 09:12:10 +0800

    Initial Commit

---
+++ 00781045_HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00781045_HW2
+//
+//  Created by User09 on 2020/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00781045_HW2/_0781045_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  _0781045_HW2App.swift
+//  00781045_HW2
+//
+//  Created by User09 on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct _0781045_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
